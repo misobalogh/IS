fedf90615ba08df0e8075d2026efc1b86a815881
JestrabikR <jestrab2003@gmail.com>
2024-02-28 21:26:50 +0100

    feat: added EF dependency and basic ApplicationDbContext

---
+++ project.DAL/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using project.DAL.Entities;
+
+namespace project.DAL;
+
+public class ApplicationDbContext(DbContextOptions contextOptions, bool seedDemoData = false)
+    : DbContext(contextOptions)
+{
+    public DbSet<ActivityEntity> Activities => Set<ActivityEntity>();
+    public DbSet<EvaluationEntity> Evaluations => Set<EvaluationEntity>();
+    public DbSet<StudentEntity> Students => Set<StudentEntity>();
+    public DbSet<SubjectEntity> Subjects => Set<SubjectEntity>();
+    public DbSet<TeacherEntity> Teachers => Set<TeacherEntity>();
+}
