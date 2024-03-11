e784cbeefbc8df8cdc42d47f3ad87a6cc44643b5
JestrabikR <jestrab2003@gmail.com>
2024-03-11 09:36:03 +0100

    feat: added IRepository and Repository

---
+++ project.DAL/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using project.DAL.Entities;
+
+namespace project.DAL.Repositories;
+
+public interface IRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    IQueryable<TEntity> Get();
+    Task DeleteAsync(Guid entityId);
+    ValueTask<bool> ExistsAsync(TEntity entity);
+    TEntity Insert(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+}
