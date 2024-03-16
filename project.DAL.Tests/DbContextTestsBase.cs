895bf6beea3730305000012b00f5c52b1f3e2e7e
Tomáš Barták <xbarta51@vutbr.cz>
2024-03-16 15:51:11 +0000

    Merged PR 15: feat: Added Base for DAL tests
    feat: Added Base for DAL tests + XUnitTestOutputConverter class #28
    
    Related work items: #28

---
+++ project.DAL.Tests/DbContextTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// using project.DAL;
+using project.DAL.Factories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace project.DAL.Tests;
+
+public class DbContextTestsBase : IAsyncLifetime
+{
+    protected DbContextTestsBase(ITestOutputHelper output)
+    {
+        XUnitTestOutputConverter converter = new(output); // XUnitTestOutputConverter is a custom class
+        Console.SetOut(converter);
+
+        DbContextFactory = new DbContextSqLiteFactory(GetType().FullName!, seedTestingData: true);
+        ProjectDbContextSUT = DbContextFactory.CreateDbContext();
+    }
+
+    protected IDbContextFactory<ApplicationDbContext> DbContextFactory { get; }
+    protected ApplicationDbContext ProjectDbContextSUT { get; }
+
+
+    public async Task InitializeAsync()
+    {
+        await ProjectDbContextSUT.Database.EnsureDeletedAsync();
+        await ProjectDbContextSUT.Database.EnsureCreatedAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await ProjectDbContextSUT.Database.EnsureDeletedAsync();
+        await ProjectDbContextSUT.DisposeAsync();
+    }
+}
