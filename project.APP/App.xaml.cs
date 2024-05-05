ae222703622bbb39c26cda8d8197d482a00ecd53
Martin Knor <martinko.knor@gmail.com>
2024-05-05 18:21:50 +0200

    feat: Added new MAUI project.

---
+++ project.APP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace project.APP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ project.APP/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace project.APP
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
