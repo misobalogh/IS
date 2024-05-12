d72e46c9a4c8ca18e191aab9bd2d9fb953372bae
Martin Knor <martinko.knor@gmail.com>
2024-05-12 22:36:16 +0200

    feat: StudentTests page structure done. ActivityModel needs fixing

--- project.App/ViewModels/StudentViewModels/StudentTestsViewModel.cs
+++ project.App/ViewModels/StudentViewModels/StudentTestsViewModel.cs
@@ -1,12 +1,35 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using project.App.Services;
 using project.App.Views.StudentViews;
+using project.BL.Facades.Interfaces;
+using project.BL.Models;
 
 namespace project.App.ViewModels;
 
-public partial class StudentTestsViewModel(IMessengerService messengerService) : ViewModelBase(messengerService)
+public partial class StudentTestsViewModel(IActivityFacade activityFacade, IMessengerService messengerService) : ViewModelBase(messengerService)
 {    //TODO NOT IMPLEMENTED
+
+    public IEnumerable<ActivityListModel> Activities { get; set; } = null!;
+
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+        Activities = await activityFacade.GetAsync();
+        //TODO filter activities that are only TESTS
+    }
+
+    //[ObservableProperty]
+    //ObservableCollection<string> tests;
+
+    ////TODO temporary way to add new tests delete later
+    //[RelayCommand]
+    //void TempADD()
+    //{
+    //    Console.WriteLine("kys\n");
+    //}
+
 
     [RelayCommand]
     async Task GoToStudentProfile()
