20aff6a4e56300b1309898a1507307627cf9992c
Dominik <dominikborek@seznam.cz>
2024-05-14 21:01:46 +0200

    feat: edit button

--- project.App/Views/StudentViews/StudentRegistrationView.xaml.cs
+++ project.App/Views/StudentViews/StudentRegistrationView.xaml.cs
@@ -4,8 +4,28 @@ namespace project.App.Views.StudentViews;
 
 public partial class StudentRegistrationView
 {
-	public StudentRegistrationView(StudentRegistrationViewModel viewModel) : base(viewModel)
+    private bool isRegistered = false;
+
+    public StudentRegistrationView(StudentRegistrationViewModel viewModel) : base(viewModel)
 	{
 		InitializeComponent();
 	}
+    private void OnRegisterButtonClicked(object sender, EventArgs e)
+    {
+        var button = sender as Button;
+        if (button != null)
+        {
+            if (isRegistered)
+            {
+                button.Text = "Register";
+                button.BackgroundColor = Color.FromRgb(171,147,227);
+            }
+            else
+            {
+                button.Text = "Registered";
+                button.BackgroundColor = Color.FromRgb(255,0,0);
+            }
+            isRegistered = !isRegistered;
+        }
+    }
 }
