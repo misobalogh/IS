e25b61775f3c7946f323fac6cced18b1df7146dc
Michal Balogh <baloghmichal03@gmail.com>
2024-05-15 18:58:31 +0200

    feat: displays only tests for signed user

--- project.App/ViewModels/StudentViewModels/StudentTestsViewModel.cs
+++ project.App/ViewModels/StudentViewModels/StudentTestsViewModel.cs
@@ -3,23 +3,37 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using project.App.Services;
 using project.App.Views.StudentViews;
+using project.BL.Facades;
 using project.BL.Facades.Interfaces;
 using project.BL.Models;
+using project.DAL.Enums;
 
 namespace project.App.ViewModels;
 
 public partial class StudentTestsViewModel(
     IActivityFacade activityFacade,
+    IEnrolledSubjectsFacade enrolledSubjectsFacade,
     IMessengerService messengerService,
     StudentDataService studentDataService) : StudentNavigationSideBar(messengerService, studentDataService)
 {    
-    public IEnumerable<ActivityListModel> Activities { get; set; } = null!;
+    public IEnumerable<ActivityListModel> Activities { get; private set; } = null!;
+    public IEnumerable<EnrolledSubjectsListModel> EnrolledSubjects { get; private set; } = null!;
 
     protected override async Task LoadDataAsync()
     {
         await base.LoadDataAsync();
         Activities = await activityFacade.GetAsync();
-        //TODO filter activities that are only TESTS
+        EnrolledSubjects = await enrolledSubjectsFacade.GetAsync();
+
+        if (loggedUser != null)
+        {
+            EnrolledSubjects = EnrolledSubjects.Where(subject => subject.StudentId == loggedUser.Id);
+            List<Guid> EnrolledSubjecsIds = EnrolledSubjects.Select(subject => subject.SubjectId).ToList();
+            
+            Activities = Activities.Where(activity =>
+                (activity.ActivityType == ActivityType.MidtermExam || activity.ActivityType == ActivityType.FinalExam) 
+                && EnrolledSubjecsIds.Contains(activity.SubjectId));
+        }
     }
 }
 
