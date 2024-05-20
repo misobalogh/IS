90cb0123b10293856b983dfd52a679164f91d9fe
JestrabikR <jestrab2003@gmail.com>
2024-05-21 00:11:22 +0200

    feat: added UI for subject sorting in student view

--- project.App/ViewModels/StudentViewModels/StudentSubjectsViewModel.cs
+++ project.App/ViewModels/StudentViewModels/StudentSubjectsViewModel.cs
@@ -14,6 +14,19 @@ public partial class StudentSubjectsViewModel(
 {    
     public IEnumerable<SubjectListModel> Subjects { get; set; } = null!;
 
+    public string SubjectNameBtn { get; set; } = "Name";
+    public string SubjectTagBtn { get; set; } = "Tag";
+
+    private bool SortDescending = true;
+
+    private SortBy SortedBy = SortBy.None;
+
+    private enum SortBy {
+        Name,
+        Tag,
+        None
+    };
+
     protected override async Task LoadDataAsync()
     {
         await base.LoadDataAsync();
@@ -25,6 +38,72 @@ public partial class StudentSubjectsViewModel(
     async Task Search(string searchTerm)
     {
         Subjects = await subjectFacade.SearchSubject(searchTerm);
+    }
+
+    [RelayCommand]
+    void SortByName()
+    {
+        ResetToOriginalName();
+        
+        ChangeSortDirectionIfSorted(SortBy.Name);
+
+        SubjectNameBtn = GetSortColName(SubjectNameBtn);
+
+        //TODO: seradit seznam studentu
+
+        SortedBy = SortBy.Name;
+    }
+
+    [RelayCommand]
+    void SortByTag()
+    {
+        ResetToOriginalName();
+        
+        ChangeSortDirectionIfSorted(SortBy.Tag);
+        
+        SubjectTagBtn = GetSortColName(SubjectTagBtn);
+
+        //TODO: seradit seznam studentu
+
+        SortedBy = SortBy.Tag;
+    }
+
+    /// <summary>
+    /// Sets column header to original text - without arrows
+    /// </summary>
+    private void ResetToOriginalName()
+    {
+        SubjectNameBtn = "Name";
+        SubjectTagBtn = "Tag";
+    }
+
+    /// <summary>
+    /// if clicked on column by which is already sorted -> change direction of sort
+    /// </summary>
+    private void ChangeSortDirectionIfSorted(SortBy newSortBy)
+    {
+        if (newSortBy == SortBy.Name && SortedBy == SortBy.Name)
+        {
+            SortDescending = !SortDescending;
+        } else if (newSortBy == SortBy.Tag && SortedBy == SortBy.Tag)
+        {
+            SortDescending = !SortDescending;
+        } else // if switched to sorting by different column, set to descending
+        {
+            SortDescending = true;
+        }
+    }
+
+    private string GetSortColName(string colName)
+    {
+        if (SortDescending)
+        {
+            return colName + " ↓";
+        }
+        else
+        {
+            return colName + " ↑";
+        }
     }
 }
 
