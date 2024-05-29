777bff9ce3f924b96483a2b0313320d1e1c315fa
JestrabikR <jestrab2003@gmail.com>
2024-05-29 16:34:45 +0200

    feat: student subjects registration view

---
+++ project.APP/ViewModels/StudentViewModels/StudentSubjectsRegistrationViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using project.App.Services;
+using project.App.Views.StudentViews;
+using project.BL.Facades;
+using project.BL.Facades.Interfaces;
+using project.BL.Models;
+using project.DAL.Enums;
+
+namespace project.App.ViewModels;
+
+public partial class StudentSubjectsRegistrationViewModel(
+    ISubjectFacade subjectFacade,
+    IMessengerService messengerService,
+    UserDataService userDataService) : StudentNavigationSideBar(messengerService, userDataService)
+{
+
+    public IEnumerable<SubjectListModel> Subjects { get; set; } = null!;
+
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+        Subjects = await subjectFacade.GetAsync();
+    }
+
+    [RelayCommand]
+    async Task Register()
+    {
+        //TODO
+    }
+}
+
