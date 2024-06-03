c244dad4881e3dad59b02f28db76d0d72b420886
Michal Balogh <baloghmichal03@gmail.com>
2024-06-03 17:07:39 +0200

    feat: extraction of the side nav bar to separate component

---
+++ project.APP/Views/AdminViews/AdminCommon/AdminSideNavBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace project.App.Views.AdminViews;
+
+public partial class AdminSideNavBar : VerticalStackLayout
+{
+    public AdminSideNavBar()
+    {
+        InitializeComponent();
+    }
+}
