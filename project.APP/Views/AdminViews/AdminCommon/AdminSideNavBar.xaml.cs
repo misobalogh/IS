e4f35807df6bb3da52e798826903d603a58cf9b8
Michal Balogh <baloghmichal03@gmail.com>
2024-06-03 17:14:54 +0200

    feat: better side nav bar, now acitve view can be binded to the buttons

--- project.APP/Views/AdminViews/AdminCommon/AdminSideNavBar.xaml.cs
+++ project.APP/Views/AdminViews/AdminCommon/AdminSideNavBar.xaml.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.Maui.Controls;
 
 namespace project.App.Views.AdminViews;
-
 public partial class AdminSideNavBar : VerticalStackLayout
 {
+    public static readonly BindableProperty ActiveViewProperty =
+        BindableProperty.Create(nameof(ActiveView), typeof(string), typeof(AdminSideNavBar), string.Empty);
+
+    public string ActiveView
+    {
+        get => (string)GetValue(ActiveViewProperty);
+        set => SetValue(ActiveViewProperty, value);
+    }
+
     public AdminSideNavBar()
     {
         InitializeComponent();
